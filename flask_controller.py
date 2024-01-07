1f093be129ca3d486fd2e2f67a62393f1173a92f
JCaeta <63880187+JCaeta@users.noreply.github.com>
2024-01-07 00:51:47 -0300

    Commit 2024-01-07

--- flask_controller.py
+++ flask_controller.py
@@ -3,28 +3,24 @@ from flask_cors import CORS
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"]) # Client http
+CORS(app, origins=["*"])
 app.config['CORS HEADERS'] = 'Content-Type'
-socketio = SocketIO(app)
-
-@app.route('/')
-def handle_message():
-    return "Hello world"
-
-# @socketio.on('message')
-# def handle_message(msg):
-#     print("Message: ", msg)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @socketio.on('message')
 def handle_message(msg):
     print("Received message:", msg)
+    
     # Simulate a time-consuming task (replace this with your actual task)
     import time
-    time.sleep(5)
+    time.sleep(2)
     result = "Task completed: " + msg
     print("Sending result:", result)
     socketio.emit('result', {'data': result})
 
 if __name__ == '__main__':
     # app.run(debug = True)
-    socketio.run(app)
+    socketio.run(app, debug = True)
+    
+    
+    
