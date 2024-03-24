836c35ea214337974c6f22bb39702291c10b0ad7
JCaeta <63880187+JCaeta@users.noreply.github.com>
2024-03-24 12:29:06 -0300

    Commit 2024-03-24

---
+++ datetimes.py
@@ -0,0 +1,14 @@
+# Sort string datetimes
+datetimes = [
+    '2023-04-16 15:47',
+    '2023-04-17 13:12', 
+    '2023-04-16 14:48',
+    '2023-04-16 18:39',
+    '2023-04-17 12:12', 
+]
+
+sorted_datetimes = sorted(datetimes, key=lambda x: (x[:10], x[11:]))
+print(sorted_datetimes)
+
+
+
